bf158d7e54cca8377c425a16c3e17fd91806d0fa
Markus B <markus@example.com>
2014-06-01 13:55:36 +0200

    nix config

---
+++ configuration.nix
@@ -0,0 +1,73 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless.
+
+  # Select internationalisation properties.
+   i18n = {
+     consoleFont = "lat9w-16";
+     consoleKeyMap = "de";
+     defaultLocale = "de_DE.UTF-8";
+   };
+
+  # List packages installed in system profile. To search by name, run:
+  # -env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+     wget zsh thunderbird emacs vim
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "de";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.kdm.enable = true;
+  services.xserver.desktopManager.kde4.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+	
+  services.virtualbox.enable = true;
+
+  fileSystems = [
+   { mountPoint = "/";
+     label = "nixos";  
+   }
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.guest = {
+    name = "markus";
+    group = "users";
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    createHome = true;
+    home = "/home/markus";
+    shell = "/run/current-system/sw/bin/bash";
+   };
+
+}
+
